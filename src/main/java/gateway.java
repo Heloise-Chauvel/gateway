c6e33aa762acef94f7ee1d66056d1c25b7561ceb
Héloïse Chauvel <chauvel.h@gmail.com>
2018-04-13 13:32:39 +0200

    Squelette class

---
+++ src/main/java/gateway.java
@@ -0,0 +1,11 @@
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/api/test")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class gateway {
+
+}
